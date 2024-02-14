8fd47d5c3f9792448cd5c9d5567ba50a4ecae738
Alex Koenig <alex.koenig4@gmail.com>
2024-02-13 23:02:25 -0500

    feat: define basic schema

---
+++ src/db/schema.ts
@@ -0,0 +1,26 @@
+import { pgTable, serial, text, integer } from "drizzle-orm/pg-core";
+
+export const regions = pgTable("regions", {
+  region_id: serial("region_id").primaryKey(),
+  name: text("name"),
+});
+
+export const cities = pgTable("cities", {
+  city_id: serial("city_id").primaryKey(),
+  name: text("name"),
+  region_id: integer("region_id").references(() => regions.region_id),
+  current_year: integer("current_year"),
+});
+
+export const venues = pgTable("venues", {
+  venue_id: serial("venue_id").primaryKey(),
+  name: text("name"),
+  city_id: integer("city_id").references(() => cities.city_id),
+});
+
+export const starAwards = pgTable("star_awards", {
+  star_award_id: serial("star_award_id").primaryKey(),
+  year: integer("year"),
+  venue_id: integer("venue_id").references(() => venues.venue_id),
+  stars: integer("stars"),
+});
