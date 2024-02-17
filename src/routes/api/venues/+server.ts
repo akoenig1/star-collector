05609b1bf59e546b928439e339d6ccde97e695d4
Alex Koenig <alex.koenig4@gmail.com>
2024-02-17 00:16:27 -0500

    feat: basic CRUD for venues

---
+++ src/routes/api/venues/+server.ts
@@ -0,0 +1,77 @@
+import { eq } from 'drizzle-orm';
+import { db } from '$lib/db/db';
+import { venues } from '$lib/db/schema';
+import { Venue, NewVenue } from '$lib/db/types';
+
+
+// GET /api/venues
+export async function GET() {
+  try {
+    const result: Venue[] = await db.select().from(venues);
+
+    return new Response(JSON.stringify(result), { status: 200 });
+  } catch (error) {
+    return new Response('Internal Server Error', { status: 500 });
+  }
+}
+
+// POST /api/venues
+export async function POST({ request }) {
+  try {
+    const data = await request.formData();
+    const venueName = data.get('name') as string;
+    const cityId = data.get('city_id') as string;
+    const venue: NewVenue = { 
+      name: venueName,
+      city_id: parseInt(cityId),
+    };
+
+    const result = await db.insert(venues).values(venue).returning();
+
+    return new Response(JSON.stringify(result), { status: 201 });
+  } catch (error) {
+    return new Response('Internal Server Error', { status: 500 });
+  }
+}
+
+// PUT /api/venues/:id
+export async function PUT({ request, url }) {
+  try {
+    const params = url.searchParams;
+    const idToUpdate = parseInt(params.get('id') as string);
+
+    const data = await request.formData();
+    const updatedVenueName = data.get('name') as string | null;
+    const updatedCityId = data.get('city_id') as string | null;
+
+    const updatedVenue = await db.update(venues)
+      .set({
+        // TODO: Are these ternaries safe? Testing seems to indicate that they are.
+        name: updatedVenueName ? updatedVenueName : undefined,
+        city_id: updatedCityId ? parseInt(updatedCityId) : undefined,
+      })
+      .where(eq(venues.city_id, idToUpdate))
+      .returning();
+    
+    return new Response(JSON.stringify(updatedVenue), { status: 201 });
+  } catch (error) {
+    return new Response('Internal Server Error', { status: 500 });
+  }
+}
+
+// DELETE /api/venues/:id
+export async function DELETE({ url }) {
+  try {
+    const params = url.searchParams;
+    const idToDelete = parseInt(params.get('id') as string);
+
+    console.log(`Deleting venue with id: ${idToDelete}`);
+    const deletedVenueId = await db.delete(venues)
+      .where(eq(venues.venue_id, idToDelete))
+      .returning({deletedId: venues.venue_id});
+    
+    return new Response(JSON.stringify(deletedVenueId), { status: 201 });
+  } catch (error) {
+    return new Response('Internal Server Error', { status: 500 });
+  }
+}
