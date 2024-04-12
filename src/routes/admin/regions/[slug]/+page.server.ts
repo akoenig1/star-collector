48c720cece97506d80f94712cd6218c91cb31db3
Alex Koenig <alex.koenig4@gmail.com>
2024-04-12 18:17:28 -0400

    feat: display cities in region

--- src/routes/admin/regions/[slug]/+page.server.ts
+++ src/routes/admin/regions/[slug]/+page.server.ts
@@ -2,22 +2,38 @@ import { error } from "@sveltejs/kit";
 import { eq } from "drizzle-orm";
 
 import { db } from '$lib/db/db';
-import { Region } from "$lib/db/types";
-import { regions } from '$lib/db/schema';
+import { City, Region } from "$lib/db/types";
+import { cities, regions } from '$lib/db/schema';
 
 export async function load(event) {
 	const { params } = event;
   const { slug } = params;
 
-  const result: Region[] = await db.select()
-    .from(regions)
-    .where(eq(regions.slug, slug))
-
-  const region = result[0];
+  const region = await getRegion(slug);
+  const cities = await getCities(region.region_id);
 
 	if (region) {
-		return { region: region };
+		return { 
+      region: region,
+      cities: cities,
+    };
 	}
 
 	error(404, 'Not found');
+}
+
+const getRegion = async (slug: string) => {
+  const result: Region[] = await db.select()
+    .from(regions)
+    .where(eq(regions.slug, slug));
+
+  return result[0];
+}
+
+const getCities = async (regionId: number) => {
+  const result: City[] = await db.select()
+    .from(cities)
+    .where(eq(cities.region_id, regionId));
+
+  return result;
 }
--- src/routes/admin/regions/[slug]/+page.svelte
+++ src/routes/admin/regions/[slug]/+page.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
   export let data;
-  const { region } = data;
+  const { region, cities } = data;
 </script>
 
 <h1>{region.name}</h1>
+
+<ul>
+  {#each cities as city}
+    <li>
+      <a href="/admin/cities/{city.slug}">{city.name}</a>
+    </li>
+  {/each}
+</ul>
