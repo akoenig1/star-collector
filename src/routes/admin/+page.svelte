998c681327d4e2984feb1deb9dcf58bf3d981d99
Alex Koenig <alex.koenig4@gmail.com>
2024-04-12 18:58:37 -0400

    feat: links to basic admin dashboard routes

---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,9 @@
+<h1>Admin Dashboaard</h1>
+
+<ul>
+  <a href="/admin/regions">Regions</a>
+  <a href="/admin/cities">Cities (TBD)</a>
+  <a href="/admin/venues">Venues (TBD)</a>
+  <a href="/admin/star-awards">Star Awards (TBD)</a>
+  <a href="/admin/users">Users (TBD)</a>
+</ul>
