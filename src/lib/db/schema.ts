86b307035e5dad5eac97382047404d8ff8109942
Alex Koenig <alex.koenig4@gmail.com>
2024-04-11 23:23:18 -0400

    feat: add users and sessions tables

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,4 +1,19 @@
-import { pgTable, serial, text, integer } from "drizzle-orm/pg-core";
+import { pgTable, serial, text, integer, timestamp } from "drizzle-orm/pg-core";
+
+export const users = pgTable("user", {
+	id: text("id").primaryKey()
+});
+
+export const sessions = pgTable("session", {
+	id: text("id").primaryKey(),
+	userId: text("user_id")
+		.notNull()
+		.references(() => users.id),
+	expiresAt: timestamp("expires_at", {
+		withTimezone: true,
+		mode: "date"
+	}).notNull()
+});
 
 export const regions = pgTable("regions", {
   region_id: serial("region_id").primaryKey(),
