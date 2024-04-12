b82880a0e3b8cc493883839fcb41a7f53bbb17e9
Alex Koenig <alex.koenig4@gmail.com>
2024-04-12 16:13:49 -0400

    feat: add slugs to regions cities and venues

---
+++ src/lib/db/migrations/0003_huge_big_bertha.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "cities" ADD COLUMN "slug" text;--> statement-breakpoint
+ALTER TABLE "regions" ADD COLUMN "slug" text;--> statement-breakpoint
+ALTER TABLE "venues" ADD COLUMN "slug" text;
