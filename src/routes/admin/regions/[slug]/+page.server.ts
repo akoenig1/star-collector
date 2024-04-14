b16123883206cb57f7b778e09c776d263b2a0d40
Alex Koenig <alex.koenig4@gmail.com>
2024-04-13 23:44:27 -0400

    feat: add city

--- src/routes/admin/regions/[slug]/+page.server.ts
+++ src/routes/admin/regions/[slug]/+page.server.ts
@@ -1,9 +1,14 @@
 import { error } from "@sveltejs/kit";
 import { eq } from "drizzle-orm";
+import { fail, superValidate } from "sveltekit-superforms";
+import { zod } from "sveltekit-superforms/adapters";
+import { z } from "zod";
 
 import { db } from '$lib/db/db';
-import { City, Region } from "$lib/db/types";
+import { City, Region, NewCity } from "$lib/db/types";
 import { cities, regions } from '$lib/db/schema';
+import { Actions } from "./$types";
+
 
 export async function load(event) {
 	const { params } = event;
@@ -11,16 +16,54 @@ export async function load(event) {
 
   const region = await getRegion(slug);
   const cities = await getCities(region.region_id);
+  const form = await superValidate(zod(cityFormSchema));
 
 	if (region) {
 		return { 
-      region: region,
-      cities: cities,
+      region,
+      cities,
+      form,
     };
 	}
 
 	error(404, 'Not found');
 }
+
+export const actions: Actions = {
+  create: async ({ params, request }) => {
+    const form = await superValidate(request, zod(cityFormSchema));
+    const { slug } = params;
+    // This feels wildly inefficient. Either svelte is ass or I have no clue how to use it
+    const region = await getRegion(slug);
+
+    if (!form.valid) {
+      return fail(400, { form });
+    }
+
+    const { cityName, slug: citySlug, currentYear } = form.data;
+
+    const newCity: NewCity = {
+      name: cityName,
+      region_id: region!.region_id,
+      current_year: currentYear,
+      slug: citySlug ?? cityName.toLowerCase().replace(/\s/g, '-'),
+    }
+
+    const result = await addCity(newCity);
+
+    if (result.success) {
+      return { form }
+    } else {
+      error(500, result.message)
+    }
+  }
+};
+
+const cityFormSchema = z.object({
+  cityName: z.string(),
+  slug: z.string().optional(),
+  currentYear: z.number(),
+});
 
 const getRegion = async (slug: string) => {
   const result: Region[] = await db.select()
@@ -36,4 +79,25 @@ const getCities = async (regionId: number) => {
     .where(eq(cities.region_id, regionId));
 
   return result;
+}
+
+const addCity = async (newCity: NewCity) => {
+  try { 
+    console.log(`Adding city ${newCity.name}...`)
+    const res = await db.insert(cities).values(newCity).returning();
+    const city = res[0];
+    console.log(`City ${city.name} added successfully!`)
+    return {
+      success: true,
+      message: `City ${city.name} added successfully!`,
+    }
+  } catch (error) {
+    console.error(error);
+    let detail = '';
+    if (error instanceof Error) detail = error.message;
+    return {
+      success: false,
+      message: `Error adding city ${newCity.name}: ${detail}`,
+    }
+  }
 }
--- src/routes/admin/regions/[slug]/+page.svelte
+++ src/routes/admin/regions/[slug]/+page.svelte
@@ -1,9 +1,21 @@
 <script lang="ts">
+  import { superForm } from 'sveltekit-superforms';
+  
   export let data;
-  const { region, cities } = data;
+  const { region } = data;
+  $: cities = data.cities;
+
+  const { form, errors, constraints, message, enhance } = superForm(data.form, {
+    onError({ result }) {
+      console.log(result);
+      $message = result.error.message || "Unknown error";
+    }
+  });
 </script>
 
 <h1>{region.name}</h1>
+
+<p>Showing {cities.length} cities.</p>
 
 <ul>
   {#each cities as city}
@@ -11,4 +23,38 @@
       <a href="/admin/cities/{city.slug}">{city.name}</a>
     </li>
   {/each}
+  <form action="?/create" method="post" use:enhance>
+    <label for="cityName">City Name</label>
+    <input 
+      name="cityName" 
+      id="city-name" 
+      aria-invalid={$errors.cityName ? 'true' : undefined}
+      bind:value={$form.cityName} 
+      {...$constraints.cityName}
+    />
+    {#if $errors.cityName}<span class="invalid">{$errors.cityName}</span>{/if}
+
+    <label for="slug">Slug</label>
+    <input 
+      name="slug" 
+      id="slug" 
+      aria-invalid={$errors.slug ? 'true' : undefined}
+      bind:value={$form.slug} 
+      {...$constraints.slug}
+    />
+    {#if $errors.slug}<span class="invalid">{$errors.slug}</span>{/if}
+
+    <label for="currentYear">Current Year</label>
+    <input 
+      name="currentYear" 
+      id="current-year" 
+      aria-invalid={$errors.currentYear ? 'true' : undefined}
+      bind:value={$form.currentYear} 
+      {...$constraints.currentYear}
+    />
+    {#if $errors.currentYear}<span class="invalid">{$errors.currentYear}</span>{/if}
+
+    <button type="submit">Add City</button>
+    {#if $message}<p>{$message}</p>{/if}
+  </form>
 </ul>
