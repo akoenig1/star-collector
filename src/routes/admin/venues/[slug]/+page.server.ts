01b51dc85a972173f15f9c3be8447a68c2d62a80
Alex Koenig <alex.koenig4@gmail.com>
2024-05-04 22:14:30 -0400

    feat: add star award form on venue admin page

--- src/routes/admin/venues/[slug]/+page.server.ts
+++ src/routes/admin/venues/[slug]/+page.server.ts
@@ -1,9 +1,13 @@
 import { error } from "@sveltejs/kit";
 import { eq } from "drizzle-orm";
+import { fail, superValidate } from "sveltekit-superforms";
+import { zod } from "sveltekit-superforms/adapters";
+import { z } from "zod";
 
 import { db } from '$lib/db/db';
-import { Venue, StarAward } from "$lib/db/types";
+import { Venue, StarAward, NewStarAward } from "$lib/db/types";
 import { venues, starAwards } from '$lib/db/schema';
+import { Actions } from "./$types";
 
 export async function load(event) {
 	const { params } = event;
@@ -11,16 +15,53 @@ export async function load(event) {
 
   const venue = await getVenue(slug);
   const starAwards = await getStarAwards(venue.venue_id);
+  const form = await superValidate(zod(starAwardFormSchema));
+
 
 	if (venue) {
 		return { 
       venue: venue,
       starAwards: starAwards,
+      form,
     };
 	}
 
 	error(404, 'Not found');
 }
+
+export const actions: Actions = {
+  create: async ({ params, request }) => {
+    const form = await superValidate(request, zod(starAwardFormSchema));
+    const { slug } = params;
+    const venue = await getVenue(slug);
+    const { venue_id, name: venueName } = venue;
+
+    if (!form.valid) {
+      return fail(400, { form });
+    }
+
+    const { starAwardYear, stars } = form.data;
+
+    const newStarAward: NewStarAward = {
+      year: starAwardYear,
+      venue_id: venue_id,
+      stars: stars,
+    }
+
+    const result = await addStarAward(newStarAward, venueName!);
+
+    if (result.success) {
+      return { form }
+    } else {
+      error(500, result.message)
+    }
+  }
+};
+
+const starAwardFormSchema = z.object({
+  starAwardYear: z.number(),
+  stars: z.number().min(1).max(3),
+});
 
 const getVenue = async (slug: string) => {
   const result: Venue[] = await db.select()
@@ -36,4 +77,25 @@ const getStarAwards = async (venueId: number) => {
     .where(eq(starAwards.venue_id, venueId));
 
   return result;
+}
+
+const addStarAward = async (newStarAward: NewStarAward, venueName: string) => {
+  try { 
+    console.log(`Adding ${venueName} Star Award ${newStarAward.year}...`)
+    const res = await db.insert(starAwards).values(newStarAward).returning();
+    const starAward = res[0];
+    console.log(`${venueName} Star Award ${starAward.year} added successfully!`)
+    return {
+      success: true,
+      message: `${venueName} Star Award ${starAward.year} added successfully!`,
+    }
+  } catch (error) {
+    console.error(error);
+    let detail = '';
+    if (error instanceof Error) detail = error.message;
+    return {
+      success: false,
+      message: `Error adding ${venueName} Star Award ${newStarAward.year}: ${detail}`,
+    }
+  }
 }
--- src/routes/admin/venues/[slug]/+page.svelte
+++ src/routes/admin/venues/[slug]/+page.svelte
@@ -1,19 +1,52 @@
 <script lang="ts">
+  import { superForm } from 'sveltekit-superforms';
+  
   export let data;
-  const { venue, starAwards } = data;
+  const { venue } = data;
+  $: starAwards = data.starAwards;
+
+  const { form, errors, constraints, message, enhance } = superForm(data.form, {
+    onError({ result }) {
+      console.log(result);
+      $message = result.error.message || "Unknown error";
+    }
+  });
 </script>
 
 <h1>{venue.name}</h1>
+
+<p>Showing {starAwards.length} star awards.</p>
 
 <ul>
   {#each starAwards as starAward}
     <li>
       <div>
-        {starAward.year}
-      </div>
-      <div>
-        {starAward.stars}
+        {starAward.year}: {starAward.stars}
       </div>
     </li>
   {/each}
+  <form action="?/create" method="post" use:enhance>
+    <label for="starAwardYear">Star Award Year</label>
+    <input 
+      name="starAwardYear" 
+      id="star-award-year" 
+      aria-invalid={$errors.starAwardYear ? 'true' : undefined}
+      bind:value={$form.starAwardYear} 
+      {...$constraints.starAwardYear}
+    />
+    {#if $errors.starAwardYear}<span class="invalid">{$errors.starAwardYear}</span>{/if}
+
+    <label for="stars">Stars</label>
+    <input 
+      name="stars" 
+      id="stars" 
+      aria-invalid={$errors.stars ? 'true' : undefined}
+      bind:value={$form.stars} 
+      {...$constraints.stars}
+    />
+    {#if $errors.stars}<span class="invalid">{$errors.stars}</span>{/if}
+
+    <button type="submit">Add Star Award</button>
+    {#if $message}<p>{$message}</p>{/if}
+  </form>
 </ul>
