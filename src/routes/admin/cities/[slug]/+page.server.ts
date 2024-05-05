4b101672a996d47086d2881231830bf2050cd6f2
Alex Koenig <alex.koenig4@gmail.com>
2024-05-04 21:42:51 -0400

    feat: add new venue form to city admin page

--- src/routes/admin/cities/[slug]/+page.server.ts
+++ src/routes/admin/cities/[slug]/+page.server.ts
@@ -1,9 +1,13 @@
 import { error } from "@sveltejs/kit";
 import { eq } from "drizzle-orm";
+import { fail, superValidate } from "sveltekit-superforms";
+import { zod } from "sveltekit-superforms/adapters";
+import { z } from "zod";
 
 import { db } from '$lib/db/db';
-import { City, Venue } from "$lib/db/types";
+import { City, Venue, NewVenue } from "$lib/db/types";
 import { cities, venues } from '$lib/db/schema';
+import { Actions } from "./$types";
 
 export async function load(event) {
 	const { params } = event;
@@ -11,16 +15,51 @@ export async function load(event) {
 
   const city = await getCity(slug);
   const venues = await getVenues(city.city_id);
+  const form = await superValidate(zod(venueFormSchema));
 
 	if (city) {
 		return { 
       city: city,
       venues: venues,
+      form,
     };
 	}
 
 	error(404, 'Not found');
 }
+
+export const actions: Actions = {
+  create: async ({ params, request }) => {
+    const form = await superValidate(request, zod(venueFormSchema));
+    const { slug } = params;
+    const city = await getCity(slug);
+
+    if (!form.valid) {
+      return fail(400, { form });
+    }
+
+    const { venueName, slug: venueSlug } = form.data;
+
+    const newVenue: NewVenue = {
+      name: venueName,
+      city_id: city!.city_id,
+      slug: venueSlug ?? venueName.toLowerCase().replace(/\s/g, '-'),
+    }
+
+    const result = await addVenue(newVenue);
+
+    if (result.success) {
+      return { form }
+    } else {
+      error(500, result.message)
+    }
+  }
+};
+
+const venueFormSchema = z.object({
+  venueName: z.string(),
+  slug: z.string().optional(),
+});
 
 const getCity = async (slug: string) => {
   const result: City[] = await db.select()
@@ -36,4 +75,25 @@ const getVenues = async (cityId: number) => {
     .where(eq(venues.city_id, cityId));
 
   return result;
+}
+
+const addVenue = async (newVenue: NewVenue) => {
+  try { 
+    console.log(`Adding venue ${newVenue.name}...`)
+    const res = await db.insert(venues).values(newVenue).returning();
+    const venue = res[0];
+    console.log(`Venue ${venue.name} added successfully!`)
+    return {
+      success: true,
+      message: `Venue ${venue.name} added successfully!`,
+    }
+  } catch (error) {
+    console.error(error);
+    let detail = '';
+    if (error instanceof Error) detail = error.message;
+    return {
+      success: false,
+      message: `Error adding venue ${newVenue.name}: ${detail}`,
+    }
+  }
 }
--- src/routes/admin/cities/[slug]/+page.svelte
+++ src/routes/admin/cities/[slug]/+page.svelte
@@ -1,9 +1,22 @@
 <script lang="ts">
+  import { superForm } from 'sveltekit-superforms';
+  
   export let data;
-  const { city, venues } = data;
+  const { city } = data;
+
+  $: venues = data.venues;
+
+  const { form, errors, constraints, message, enhance } = superForm(data.form, {
+    onError({ result }) {
+      console.log(result);
+      $message = result.error.message || "Unknown error";
+    }
+  });
 </script>
 
 <h1>{city.name}</h1>
+
+<p>Showing {venues.length} venues.</p>
 
 <ul>
   {#each venues as venue}
@@ -11,4 +24,28 @@
       <a href="/admin/venues/{venue.slug}">{venue.name}</a>
     </li>
   {/each}
+  <form action="?/create" method="post" use:enhance>
+    <label for="venueName">Venue Name</label>
+    <input 
+      name="venueName" 
+      id="venue-name" 
+      aria-invalid={$errors.venueName ? 'true' : undefined}
+      bind:value={$form.venueName} 
+      {...$constraints.venueName}
+    />
+    {#if $errors.venueName}<span class="invalid">{$errors.venueName}</span>{/if}
+
+    <label for="slug">Slug</label>
+    <input 
+      name="slug" 
+      id="slug" 
+      aria-invalid={$errors.slug ? 'true' : undefined}
+      bind:value={$form.slug} 
+      {...$constraints.slug}
+    />
+    {#if $errors.slug}<span class="invalid">{$errors.slug}</span>{/if}
+
+    <button type="submit">Add Venue</button>
+    {#if $message}<p>{$message}</p>{/if}
+  </form>
 </ul>
