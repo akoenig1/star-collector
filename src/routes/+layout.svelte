723eaa0b085434482a367e6231250ea29bba207f
Alex Koenig <alex.koenig4@gmail.com>
2024-08-30 21:16:36 -0400

    chore: move logout to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,17 @@
 <script>
   import "../app.css";
+	import { enhance } from "$app/forms";
+
+  export let data;
 </script>
+
+{#if data.user}
+  <p>Hello, {data.user.email}!</p>
+  <form method="post" action="/logout" use:enhance>
+    <button>Sign out</button>
+  </form>
+{:else}
+  <p><a href="/login">Log in</a> to get started</p>
+{/if}
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,4 @@
 <script lang="ts">
-	import { enhance } from "$app/forms";
-
-  export let data;
 </script>
 
 <h1 class="text-xl">Welcome to Star Collector</h1>
-
-{#if data.user}
-  <p>Hello, {data.user.email}!</p>
-  <form method="post" use:enhance>
-    <button>Sign out</button>
-  </form>
-{:else}
-  <p><a href="/login">Log in</a> to get started</p>
-{/if}
