7fbad80f9100b156369f3e9c65a5263e1594bdf7
Alex Koenig <alex.koenig4@gmail.com>
2024-09-02 13:22:26 -0400

    feat: add form to cities index page

--- src/routes/admin/cities/+page.server.ts
+++ src/routes/admin/cities/+page.server.ts
@@ -1,22 +1,64 @@
 import { error } from "@sveltejs/kit";
+import { fail, superValidate } from "sveltekit-superforms";
+import { zod } from "sveltekit-superforms/adapters";
+import { z } from "zod";
 
 import { db } from '$lib/db/db';
-import { City, Region } from "$lib/db/types";
+import { NewCity, City, Region } from "$lib/db/types";
 import { cities, regions } from '$lib/db/schema';
+import { Actions } from "./$types";
 
 export async function load() {
   const regions = await getRegions();
   const cities = await getCities();
+  const form = await superValidate(zod(cityFormSchema));
 
 	if (regions && cities) {
 		return { 
       regions,
       cities,
+      form
     };
 	}
 
 	error(404, 'Not found');
 }
+
+export const actions: Actions = {
+  create: async ({ request }) => {
+    const form = await superValidate(request, zod(cityFormSchema));
+
+    if (!form.valid) {
+      return fail(400, { form });
+    }
+
+    console.log(form.data)
+    const { cityName, regionId, slug: citySlug, currentYear } = form.data;
+    console.log(regionId);
+
+    const newCity: NewCity = {
+      name: cityName,
+      region_id: regionId,
+      current_year: currentYear,
+      slug: citySlug ?? cityName.toLowerCase().replace(/\s/g, '-'),
+    }
+
+    const result = await addCity(newCity);
+
+    if (result.success) {
+      return { form }
+    } else {
+      error(500, result.message)
+    }
+  }
+};
+
+const cityFormSchema = z.object({
+  cityName: z.string(),
+  regionId: z.number(),
+  slug: z.string().optional(),
+  currentYear: z.number(),
+});
 
 const getRegions = async () => {
   const result: Region[] = await db.select().from(regions)
@@ -26,4 +68,25 @@ const getRegions = async () => {
 const getCities = async () => {
   const result: City[] = await db.select().from(cities)
   return result;
+}
+
+const addCity = async (newCity: NewCity) => {
+  try { 
+    console.log(`Adding city ${newCity.name}...`)
+    const res = await db.insert(cities).values(newCity).returning();
+    const city = res[0];
+    console.log(`City ${city.name} added successfully!`)
+    return {
+      success: true,
+      message: `City ${city.name} added successfully!`,
+    }
+  } catch (error) {
+    console.error(error);
+    let detail = '';
+    if (error instanceof Error) detail = error.message;
+    return {
+      success: false,
+      message: `Error adding city ${newCity.name}: ${detail}`,
+    }
+  }
 }
--- src/routes/admin/cities/+page.svelte
+++ src/routes/admin/cities/+page.svelte
@@ -1,26 +1,26 @@
 <script lang="ts">
-	// import { superForm } from 'sveltekit-superforms';
+	import { superForm } from 'sveltekit-superforms';
 
   export let data;
   $: cities = data.cities;
   $: regions = data.regions;
 
-  // const { form, errors, constraints, message, enhance } = superForm(data.form, {
-  //   onError({ result }) {
-  //     console.log(result);
-  //     $message = result.error.message || "Unknown error";
-  //   }
-  // });
+  const { form, errors, constraints, message, enhance } = superForm(data.form, {
+    onError({ result }) {
+      console.log(result);
+      $message = result.error.message || "Unknown error";
+    }
+  });
 </script>
 
-<h1>Regions</h1>
+<h1>Cities</h1>
 
 <p>Showing {cities.length} cities in {regions.length} regions.</p>
 
 <ul>
   {#each regions as region}
-    <li>
-      <a href="/admin/regions/{region.slug}" class="font-bold my-1">{region.name}</a>
+    <li class="my-4">
+      <a href="/admin/regions/{region.slug}" class="font-bold">{region.name}</a>
       <ul>
         {#each cities as city}
           {#if city.region_id === region.region_id}
@@ -33,3 +33,52 @@
     </li>
   {/each}
 </ul>
+
+<form action="?/create" method="post" use:enhance>
+  <label for="cityName">City Name</label>
+  <input 
+    name="cityName" 
+    id="city-name" 
+    aria-invalid={$errors.cityName ? 'true' : undefined}
+    bind:value={$form.cityName} 
+    {...$constraints.cityName}
+  />
+  {#if $errors.cityName}<span class="invalid">{$errors.cityName}</span>{/if}
+
+  <label for="slug">Slug</label>
+  <input 
+    name="slug" 
+    id="slug" 
+    aria-invalid={$errors.slug ? 'true' : undefined}
+    bind:value={$form.slug} 
+    {...$constraints.slug}
+  />
+  {#if $errors.slug}<span class="invalid">{$errors.slug}</span>{/if}
+
+  <label for="regionId">Region</label>
+  <select 
+    name="regionId" 
+    id="region-id" 
+    aria-invalid={$errors.regionId ? 'true' : undefined}
+    bind:value={$form.regionId} 
+    {...$constraints.regionId}
+  >
+    {#each regions as region (region.region_id)}
+      <option value={region.region_id}>{region.name}</option>
+    {/each}
+  </select>
+  {#if $errors.regionId}<span class="invalid">{$errors.regionId}</span>{/if}
+
+  <label for="currentYear">Current Year</label>
+  <input 
+    name="currentYear" 
+    id="current-year" 
+    aria-invalid={$errors.currentYear ? 'true' : undefined}
+    bind:value={$form.currentYear} 
+    {...$constraints.currentYear}
+  />
+  {#if $errors.currentYear}<span class="invalid">{$errors.currentYear}</span>{/if}
+
+  <button type="submit">Add City</button>
+  {#if $message}<p>{$message}</p>{/if}
+</form>
