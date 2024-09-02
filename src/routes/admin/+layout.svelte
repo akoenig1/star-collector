ba70341453adcdcb12e63ca401a785ff43ea5ca5
Alex Koenig <alex.koenig4@gmail.com>
2024-09-02 13:40:17 -0400

    feat: move navbar to admin layout template

---
+++ src/routes/admin/+layout.svelte
@@ -0,0 +1,9 @@
+<ul>
+  <a href="/admin/regions">Regions</a>
+  <a href="/admin/cities">Cities</a>
+  <a href="/admin/venues">Venues (TBD)</a>
+  <a href="/admin/star-awards">Star Awards (TBD)</a>
+  <a href="/admin/users">Users</a>
+</ul>
+
+<slot />
--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,9 +1,1 @@
 <h1>Admin Dashboard</h1>
-
-<ul>
-  <a href="/admin/regions">Regions</a>
-  <a href="/admin/cities">Cities</a>
-  <a href="/admin/venues">Venues (TBD)</a>
-  <a href="/admin/star-awards">Star Awards (TBD)</a>
-  <a href="/admin/users">Users</a>
-</ul>
