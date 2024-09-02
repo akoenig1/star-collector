5b1e04cf830899ed849d8e7171a46f0601c2026c
Alex Koenig <alex.koenig4@gmail.com>
2024-09-02 13:32:28 -0400

    chore: update admin dashboard text

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -2,7 +2,7 @@
 
 <ul>
   <a href="/admin/regions">Regions</a>
-  <a href="/admin/cities">Cities (TBD)</a>
+  <a href="/admin/cities">Cities</a>
   <a href="/admin/venues">Venues (TBD)</a>
   <a href="/admin/star-awards">Star Awards (TBD)</a>
   <a href="/admin/users">Users</a>
