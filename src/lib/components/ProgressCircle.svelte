16471a903bc80079ad0bdf58862e767c45389363
Alex Koenig <alex.koenig4@gmail.com>
2024-09-05 23:57:08 -0400

    fix: progress circle animation

--- src/lib/components/ProgressCircle.svelte
+++ src/lib/components/ProgressCircle.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
   export let value, maxValue;
+
+  let progressInverse = 100 - (value / maxValue * 100);
 </script>
 
 <div class="relative size-40">
   <svg class="size-full -rotate-90" viewBox="0 0 36 36">
     <circle cx="18" cy="18" r="16" fill="none" class="stroke-current text-gray-200 dark:text-neutral-700" stroke-width="2"></circle>
-    <circle cx="18" cy="18" r="16" fill="none" class="stroke-current text-blue-600 dark:text-blue-500 animate-fill" stroke-width="2" stroke-dasharray="100" stroke-dashoffset={maxValue - value} stroke-linecap="round"></circle>
+    <circle cx="18" cy="18" r="16" fill="none" class="stroke-current text-blue-600 dark:text-blue-500 animate-fill" style="--progress: {progressInverse}" stroke-width="2" stroke-dasharray="100" stroke-dashoffset={value} stroke-linecap="round"></circle>
   </svg>
 
   <div class="absolute top-1/2 start-1/2 transform -translate-y-1/2 -translate-x-1/2">
-    <span class="text-center text-2xl font-bold text-blue-600 dark:text-blue-500">{maxValue}</span>
+    <span class="text-center text-2xl font-bold text-blue-600 dark:text-blue-500">{value}</span>
   </div>
 </div>
 
@@ -19,7 +21,7 @@
       stroke-dashoffset: 100;
     }
     to {
-      stroke-dashoffset: 0;
+      stroke-dashoffset: var(--progress)
     }
   }
 
